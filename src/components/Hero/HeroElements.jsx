924dadc0c0ff712db15a06d45c90f4ae538aeba0
leovenom <leonardt@oputlook.com>
2020-12-18 16:15:24 +0100

    font

--- src/components/Hero/HeroElements.jsx
+++ src/components/Hero/HeroElements.jsx
@@ -10,6 +10,17 @@ export const HeroContainer = styled.div`
   height: 800px;
   position: relative;
   z-index: 1;
+  :before {
+    content: '';
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background: linear-gradient(180deg, rgba(0,0,0,0.2)0%, rgba(0,0,0,0.6)100%),  
+    linear-gradient(180deg, rgba(0,0,0,0.2)0%, transparent 100%)
+    z-index: 2;
+  }
 `
 export const HeroBg = styled.div`
   position: absolute;
