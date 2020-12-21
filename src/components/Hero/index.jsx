9247de5079a5d96c192bcf24ae6f14f9cbca8d43
leovenom <leonardt@oputlook.com>
2020-12-21 15:55:31 +0100

    all ok

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
         <HeroH1> Virtual Banking Make Easy</HeroH1>
         <HeroP>Sign up for a new accoun today and recive $250 in credit towards your next payment</HeroP>
         <HeroBtnWrapper>
-          <Button to='signup' onMouseEnter={onHover} onMouseLeave={onHover} primary='true' dark='true'>
+          <Button to='signup' onMouseEnter={onHover} onMouseLeave={onHover} primary='true' dark='true'               smooth={true} duration={500} spy={true} exact='true' offset={-80}>
             Get Started {hover ? <ArrowForward /> : <ArrowRight/>}
           </Button>
         </HeroBtnWrapper>
--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import ScrollToTop from '../components/ScrollToTop'
 import SignIn from '../Signin'
 
 const SigninPage = () => {
   return (
     <>
+    <ScrollToTop />
       <SignIn />
     </>
   )
