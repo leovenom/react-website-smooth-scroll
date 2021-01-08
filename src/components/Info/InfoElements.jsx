aaecee19cc2ec32e3cc7a9f56048c48488ed57b5
Leonardt Lauenstein <45887012+leovenom@users.noreply.github.com>
2021-01-08 15:50:19 +0100

    fidex
    InfoRow

--- src/components/Info/InfoElements.jsx
+++ src/components/Info/InfoElements.jsx
@@ -26,7 +26,7 @@ export const InfoRow = styled.div`
   grid-template-areas: ${({imgStart}) => (imgStart ? `'col2 col1'` : `'col1 col2'`)};
 
   @media screen and (max-width: 768px) {
-    grid-template-areas: ${({imgStart}) => (imgStart ? `'col1 col2'` : `'col1 col1' 'col2 col2'`)}
+    grid-template-areas: ${({imgStart}) => (imgStart ? `'col1' 'col2'` : `'col1 col1' 'col2 col2'`)}
   }
 `
 export const Column1 = styled.div`
